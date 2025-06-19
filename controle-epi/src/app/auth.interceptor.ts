666dacfab33926625e5e1362f8fc373d8016f312
Gabriella <gabriella.mabento@gmail.com>
2025-06-19 01:24:44 -0300

    fix: correção de cabeçalho do interception

--- controle-epi/src/app/auth.interceptor.ts
+++ controle-epi/src/app/auth.interceptor.ts
@@ -1,14 +1,15 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('token');
 
-    if(token){
+    if (token) {
         const authReq = req.clone({
-            setHeaders: {Autorization: `Bearer: ${token}`}
+            setHeaders: { Authorization: `Bearer ${token}` }
         });
-        
-        return next(authReq)
+
+        return next(authReq);
     }
-    return next(req)
-}
+
+    return next(req);
+};
